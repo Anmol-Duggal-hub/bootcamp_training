82a3728adacdd10f56e4e8115f4e64191b647189
Anmol <42423263+Anmol-Duggal-hub@users.noreply.github.com>
2020-06-01 16:45:24 -0700

    Add files via upload

---
+++ counter_java_spring_proj/src/main/java/com/anmolduggal/counter/CounterProjApplication.java
@@ -0,0 +1,13 @@
+package com.anmolduggal.counter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CounterProjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CounterProjApplication.class, args);
+	}
+
+}
---
+++ counter_java_spring_proj/src/test/java/com/anmolduggal/counter/CounterProjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anmolduggal.counter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CounterProjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
